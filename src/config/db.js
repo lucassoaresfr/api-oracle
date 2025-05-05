750445819dbc770e7cfe79b03b15c3c623e4debb
lucassoaresfr <lucasclachack@gmail.com>
2025-05-05 16:02:45 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,7 @@ async function conectarBanco() {
     const connection = await oracledb.getConnection({
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      connectString: `${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_SID}`
+      connectString: `${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_SID}` //ajeitar corretamente e jogar no docker run
     });
     return connection;
   } catch (error) {
