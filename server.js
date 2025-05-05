dec484662e9cad408c0886e77461e1196832d4cd
lucassoaresfr <lucasclachack@gmail.com>
2025-05-05 16:01:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use('/produtos', produtoRoutes);
 app.use('/count', countRoutes);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server da Api com oracle rodando na porta ${port}`)
 })
