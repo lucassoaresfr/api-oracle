0d68214d8e1bb89f651c79f611a57b8bf7f97958
lucassoaresfr <lucasclachack@gmail.com>
2025-05-06 08:27:18 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -9,6 +9,7 @@ async function conectarBanco() {
       password: process.env.DB_PASSWORD,
       connectString: process.env.DB_STRING
     });
+    
     return connection;
   } catch (error) {
     console.error('Erro ao conectar ao banco de dados:', error);
