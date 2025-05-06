eb9c1f195b7fb039a8d1aa939baa0080d817498a
lucassoaresfr <lucasclachack@gmail.com>
2025-05-06 08:23:51 -0300

    1

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,7 @@ async function conectarBanco() {
     const connection = await oracledb.getConnection({
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      connectString: `${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_SID}`
+      connectString: process.env.DB_STRING
     });
     return connection;
   } catch (error) {
